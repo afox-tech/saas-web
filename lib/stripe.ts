d88054584f54979c23a68694410f80d820e3a74c
A Tech <afox.tech999@gmail.com>
2025-07-28 02:18:50 -0700

    create pricing page

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,7 +2,6 @@ import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
     apiVersion: "2025-06-30.basil",
-    typescript: true,
 });
 
 export const getStripeSession = async ({
